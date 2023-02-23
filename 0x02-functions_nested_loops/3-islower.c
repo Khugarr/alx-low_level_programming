b2bde36bb0c5fa6006357a8cd7ee45af1c7c146c
Khugarr <tobennawilson@gmail.com>
2023-02-22 16:59:11 -0800

    update

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -9,9 +9,12 @@
  * and 0 otherwise
  */
 
- {
-if (c >= 97 && c <= 122)
-return (1);
-else
-return (0);
- }
+int _islower(int c)
+
+
+{
+     if (c >= 97 && c <= 122)
+           return (1);
+     else
+      return (0);
+}
